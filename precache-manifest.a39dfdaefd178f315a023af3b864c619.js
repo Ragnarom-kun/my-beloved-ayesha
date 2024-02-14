4ae5ae5805471881f4354994d6a3f5e4bac960dc
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2024-02-14 16:02:07 +0700

    Updates

---
+++ precache-manifest.a39dfdaefd178f315a023af3b864c619.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a007bdaab201aa5ad83d1aa261ba1691",
+    "url": "/my-beloved-ayesha/index.html"
+  },
+  {
+    "revision": "a65b4b2b04354a326f19",
+    "url": "/my-beloved-ayesha/static/css/main.3d38e28a.chunk.css"
+  },
+  {
+    "revision": "9cbde23725ea0b206ceb",
+    "url": "/my-beloved-ayesha/static/js/2.29ba7bba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-beloved-ayesha/static/js/2.29ba7bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65b4b2b04354a326f19",
+    "url": "/my-beloved-ayesha/static/js/main.dada6551.chunk.js"
+  },
+  {
+    "revision": "776a4c906f9a740b21e9",
+    "url": "/my-beloved-ayesha/static/js/runtime-main.ff2e0830.js"
+  },
+  {
+    "revision": "74680361848ef102ec88bf12318cea07",
+    "url": "/my-beloved-ayesha/static/media/my_lovely_girl.74680361.jpg"
+  },
+  {
+    "revision": "d348e4401884dd1bb421bbe513fa54a7",
+    "url": "/my-beloved-ayesha/static/media/pandinha.d348e440.gif"
+  },
+  {
+    "revision": "f1be026ff08aab1125b691c069d382f0",
+    "url": "/my-beloved-ayesha/static/media/pandinha_coracao.f1be026f.gif"
+  },
+  {
+    "revision": "46feb314ef66fd9844e6c5343e228597",
+    "url": "/my-beloved-ayesha/static/media/pandinha_fofa_cara_de_brava.46feb314.gif"
+  },
+  {
+    "revision": "8b0c4eecc314fa3131eae4e8ec70d06a",
+    "url": "/my-beloved-ayesha/static/media/pandinha_martelo.8b0c4eec.png"
+  },
+  {
+    "revision": "865f1c977c2083c42cda7c223f72ea76",
+    "url": "/my-beloved-ayesha/static/media/ursinha_chorando.865f1c97.gif"
+  },
+  {
+    "revision": "c4ff310ed4399ebe2c85ffc52ad5eeab",
+    "url": "/my-beloved-ayesha/static/media/ursinha_mordidas.c4ff310e.gif"
+  },
+  {
+    "revision": "641a584c05345c733e8e5d94f906e53c",
+    "url": "/my-beloved-ayesha/static/media/ursinho_S2_pandinha.641a584c.gif"
+  },
+  {
+    "revision": "7746f73129cc8fc7f46ada5e23f4d1b5",
+    "url": "/my-beloved-ayesha/static/media/ursinho_catucando_pandinha.7746f731.gif"
+  },
+  {
+    "revision": "ee3bbcefdd762dc554b5db84df32bded",
+    "url": "/my-beloved-ayesha/static/media/ursinho_chorando.ee3bbcef.gif"
+  },
+  {
+    "revision": "bde071c432bb0a09b34af0e0d5ffcd98",
+    "url": "/my-beloved-ayesha/static/media/ursinho_estudando.bde071c4.gif"
+  },
+  {
+    "revision": "81e2399aba3f1ff6cd0c1df8eb967412",
+    "url": "/my-beloved-ayesha/static/media/ursinho_pandinha_ok.81e2399a.gif"
+  },
+  {
+    "revision": "387281264ae4000e8a803a40169c1ac8",
+    "url": "/my-beloved-ayesha/static/media/ursinho_pegando_no_sono.38728126.gif"
+  },
+  {
+    "revision": "04b613c4523e12dcb35d7f703a752faf",
+    "url": "/my-beloved-ayesha/static/media/ursinhos_apaixonados.04b613c4.gif"
+  },
+  {
+    "revision": "e831c6c0da241c5146771d0233cfc219",
+    "url": "/my-beloved-ayesha/static/media/ursinhos_bebida.e831c6c0.gif"
+  },
+  {
+    "revision": "b887f38a5ae81d5a3fe91244955386dc",
+    "url": "/my-beloved-ayesha/static/media/ursinhos_caminhada.b887f38a.jpg"
+  },
+  {
+    "revision": "73252a13157ecdec88497a19c35256e5",
+    "url": "/my-beloved-ayesha/static/media/ursinhos_dormindo.73252a13.gif"
+  },
+  {
+    "revision": "4aa4f4ad4538a66d10b1537ea06d994c",
+    "url": "/my-beloved-ayesha/static/media/ursinhos_fotinha.4aa4f4ad.jpg"
+  }
+]);
